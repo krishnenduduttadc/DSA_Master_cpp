6e07ea434974920ebfef7e907be637fdfb57b03f
krishnendu <krishnenduduttadc@gmail.com>
2024-08-31 16:14:59 +0530

    added

--- SortingL2/Insertion.cpp
+++ SortingL2/Insertion.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 using namespace std;
 
+// void insertionSort(int arr[], int n) {
+//     for (int i = 1; i < n; i++)
+//     {
+//        int key=arr[i];
+//        int j=i-1;
+//        while(j>=0 && arr[j]>key){
+//         arr[j+1]=arr[j];
+//         j=j-1;
+//        }
+//        arr[j + 1] = key;
+//     }  
+// }
+
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++)
     {
-       int key=arr[i];
-       int j=i-1;
-       while(j>=0 && arr[j]>key){
-        arr[j+1]=arr[j];
-        j=j-1;
-       }
-       arr[j + 1] = key;
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j],arr[j-1]);
+            j--;
+        }
     }
-    
 }
 
 int main() {
