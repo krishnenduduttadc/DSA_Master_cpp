4f9ead3cb9d6f7528e9af741f7c0bbd93fad7939
krishnendu <krishnenduduttadc@gmail.com>
2024-09-07 15:23:06 +0530

    added

--- SortingL2/Merge.cpp
+++ SortingL2/Merge.cpp
@@ -1,38 +1,40 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 void merge(int arr[], int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int left[n1], right[n2];
-
-    for (int i = 0; i < n1; i++) {
-        left[i] = arr[l + i];
+    int n1=m-l+1;
+    int n2=r-m;
+    int left[n1];
+    int right[n2];
+    
+    for(int i=0;i<n1;i++){
+        left[i]=arr[l+i];
     }
-    for (int j = 0; j < n2; j++) {
-        right[j] = arr[m + 1 + j];
+    
+    for(int j=0;j<n2;j++){
+        right[j]=arr[m+1+j];
     }
-
     int i = 0, j = 0, k = l;
-    while (i < n1 && j < n2) {
-        if (left[i] <= right[j]) {
-            arr[k] = left[i];
+    
+    while(i<n1 && j<n2){
+        if(left[i]<=right[j]){
+            arr[k]=left[i];
             i++;
-        } else {
-            arr[k] = right[j];
+        }else{
+            arr[k]=right[j];
             j++;
         }
         k++;
     }
-
-    while (i < n1) {
-        arr[k] = left[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = right[j];
+    
+     while (i < n1) {
+         arr[k]=left[i];
+         i++;
+         k++;
+     }
+    
+    while(j<n2){
+        arr[k]=right[j];
         j++;
         k++;
     }
@@ -49,20 +51,22 @@ void mergeSort(int arr[], int l, int r) {
 }
 
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Given array is \n";
-    for (int i = 0; i < n; i++) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    mergeSort(arr,0,n-1);
+    
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
-    cout << "\n";
-
-    mergeSort(arr, 0, n - 1);
-
-    cout << "\nSorted array is \n";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
+    cout << endl;
     return 0;
 }
---
+++ SortingL2/QuickR.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int medianOfThree(int arr[], int l, int h) {
+    int mid = l + (h - l) / 2;
+    if (arr[l] > arr[mid])
+        swap(arr[l], arr[mid]);
+    if (arr[l] > arr[h])
+        swap(arr[l], arr[h]);
+    if (arr[mid] > arr[h])
+        swap(arr[mid], arr[h]);    
+    return mid;
+}
+
+int partition(int arr[],int l,int h){
+   
+    int medianIndex  = medianOfThree(arr, l, h);
+    swap(arr[l], arr[medianIndex]);
+    int piv=arr[l];
+    int left=l+1;
+    int right=h;
+    
+    while(left<=right){
+        while(left<=right && arr[left]<piv){
+            left++;
+        }
+        while(left<=right && arr[right]>piv){
+            right--;
+        }
+        if(left<=right){
+            swap(arr[left],arr[right]);
+            left++;
+            right--;
+            
+        }
+    }
+    swap(arr[l],arr[right]);
+    return right;
+}
+
+
+void rquicksort(int arr[], int l, int h)
+{
+    if (l < h)
+    {
+        int pivot = partition(arr, l, h);
+        rquicksort(arr, l, pivot - 1);
+        rquicksort(arr, pivot + 1, h);
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    int l = 0;
+    int h = n - 1;
+
+
+    rquicksort(arr, 0, h);
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
