c14af018300b04e7da3b0b71cd393037706320a3
krishnendu <krishnenduduttadc@gmail.com>
2024-09-14 00:43:54 +0530

    added

--- SortingL2/Selection.cpp
+++ SortingL2/Selection.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n) {
-    for(int i = 0; i < n-1; i++) {
-        int min_idx = i;
-        for(int j = i+1; j < n; j++) {
-            if(arr[j] < arr[min_idx]) {
-                min_idx = j;
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minidx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minidx])
+            {
+                minidx = j;
             }
         }
-        // Swap the found minimum element with the first element
-        int temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
+        swap(arr[i], arr[minidx]);
     }
 }
 
