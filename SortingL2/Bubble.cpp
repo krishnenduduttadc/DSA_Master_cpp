5b1e15e0d5ad3b416f1ff926504216f25f5dfe98
krishnendu <krishnenduduttadc@gmail.com>
2024-12-24 01:34:08 +0530

    add

--- SortingL2/Bubble.cpp
+++ SortingL2/Bubble.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n) {
+    int c=0;
     for(int i = 0; i < n-1; i++) {
         for(int j = 0; j < n-i-1; j++) {
             if(arr[j] > arr[j+1]) {
@@ -9,13 +10,15 @@ void bubbleSort(int arr[], int n) {
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                c++;
             }
         }
     }
+    cout<<c<<endl;
 }
 
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int arr[] = {4,3,2,1};
     int n = sizeof(arr)/sizeof(arr[0]);
     bubbleSort(arr, n);
     cout << "Sorted array: \n";
