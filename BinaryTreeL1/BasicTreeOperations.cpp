9f9fe1ce4106b4f64c6dd2358c8c66707d5a92e3
krishnendu <krishnenduduttadc@gmail.com>
2025-02-24 00:26:34 +0530

    added

--- BinaryTreeL1/BasicTreeOperations.cpp
+++ BinaryTreeL1/BasicTreeOperations.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <stack>
+
 using namespace std;
 
 // Node structure for the binary tree
@@ -16,57 +16,54 @@ struct Node {
     }
 };
 
-class BasicTreeOperations {
-public:
-    // Method to print postorder traversal of the tree
-    void printPostorder(Node* node) {
-        if (node == nullptr)
-            return;
+// Function to print postorder traversal of the tree
+void printPostorder(Node* node) {
+    if (node == nullptr)
+        return;
 
-        printPostorder(node->left);
-        printPostorder(node->right);
-        cout << node->key << " ";
+    printPostorder(node->left);
+    printPostorder(node->right);
+    cout << node->key << " ";
+}
+
+// Function to perform level order traversal
+void levelOrder(Node* root) {
+    if (root == nullptr)
+        return;
+
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        Node* curr = q.front();
+        q.pop();
+        cout << curr->key << " ";
+
+        if (curr->left != nullptr)
+            q.push(curr->left);
+        if (curr->right != nullptr)
+            q.push(curr->right);
+    }
+}
+
+// Function to find the longest root-to-leaf path
+vector<int> longestRootToLeafPath(Node* root) {
+    if (root == nullptr)
+        return vector<int>();
+
+    vector<int> longestPath;
+    vector<int> leftPath = longestRootToLeafPath(root->left);
+    vector<int> rightPath = longestRootToLeafPath(root->right);
+
+    if (leftPath.size() > rightPath.size()) {
+        longestPath = leftPath;
+    } else {
+        longestPath = rightPath;
     }
 
-    // Method to perform level order traversal
-    void levelOrder(Node* root) {
-        if (root == nullptr)
-            return;
-
-        queue<Node*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            Node* curr = q.front();
-            q.pop();
-            cout << curr->key << " ";
-
-            if (curr->left != nullptr)
-                q.push(curr->left);
-            if (curr->right != nullptr)
-                q.push(curr->right);
-        }
-    }
-
-    // Method to find the longest root-to-leaf path
-    vector<int> longestRootToLeafPath(Node* root) {
-        if (root == nullptr)
-            return vector<int>();
-
-        vector<int> longestPath;
-        vector<int> leftPath = longestRootToLeafPath(root->left);
-        vector<int> rightPath = longestRootToLeafPath(root->right);
-
-        if (leftPath.size() > rightPath.size()) {
-            longestPath = leftPath;
-        } else {
-            longestPath = rightPath;
-        }
-
-        longestPath.insert(longestPath.begin(), root->key);
-        return longestPath;
-    }
-};
+    longestPath.insert(longestPath.begin(), root->key);
+    return longestPath;
+}
 
 int main() {
     // Creating a binary tree
@@ -76,11 +73,8 @@ int main() {
     root->left->left = new Node(4);
     root->left->right = new Node(5);
 
-    // Creating an instance of BasicTreeOperations
-    BasicTreeOperations treeOps;
-
     // Printing longest root-to-leaf path
-    vector<int> longestPath = treeOps.longestRootToLeafPath(root);
+    vector<int> longestPath = longestRootToLeafPath(root);
     cout << "Longest root to leaf path: ";
     for (size_t i = 0; i < longestPath.size(); i++) {
         cout << longestPath[i];
@@ -91,12 +85,12 @@ int main() {
 
     // Printing postorder traversal
     cout << "Postorder traversal: ";
-    treeOps.printPostorder(root);
+    printPostorder(root);
     cout << endl;
 
     // Printing level order traversal
     cout << "Level order traversal: ";
-    treeOps.levelOrder(root);
+    levelOrder(root);
     cout << endl;
 
     return 0;
--- BinaryTreeL1/CheckBST.cpp
+++ BinaryTreeL1/CheckBST.cpp
@@ -3,40 +3,19 @@
 using namespace std;
 
 // Definition of a binary tree node
-struct Node
-{
+struct Node {
     int data;
     Node *left, *right;
-    Node(int data)
-    {
-        data = data;
+    Node(int data) {
+        this->data = data; // Correctly assigning data
         left = nullptr;
         right = nullptr;
     }
 };
 
-// Class to store results of isBST function
-class BSTPair
-{
-public:
-    bool isBST;
-    int min;
-    int max;
-    BSTPair(bool isBST = true, int min = INT_MAX, int max = INT_MIN)
-    {
-        this->isBST = isBST;
-        this->min = min;
-        this->max = max;
-    }
-
-    // BSTPair(bool isBST = true, int min = INT_MAX, int max = INT_MIN) : isBST(isBST), min(min), max(max) {}
-};
-
 // Function to display the binary tree (preorder traversal)
-void display(Node *node)
-{
-    if (node == nullptr)
-    {
+void display(Node *node) {
+    if (node == nullptr) {
         return;
     }
 
@@ -49,55 +28,36 @@ void display(Node *node)
     display(node->right);
 }
 
-// Function to calculate the height of the tree
-int height(Node *node)
-{
-    if (node == nullptr)
-    {
-        return -1;
-    }
-
-    int lh = height(node->left);
-    int rh = height(node->right);
-
-    return max(lh, rh) + 1;
-}
-
 // Function to check if a subtree rooted at 'node' is a BST
-BSTPair isBST(Node *node)
-{
-    if (node == nullptr)
-    {
-        return BSTPair(true, INT_MAX, INT_MIN);
+bool isBST(Node *node, int minVal, int maxVal) {
+    if (node == nullptr) {
+        return true;
+    }
+    
+    if (node->data <= minVal || node->data >= maxVal) {
+        return false;
     }
 
-    BSTPair lp = isBST(node->left);
-    BSTPair rp = isBST(node->right);
-
-    BSTPair mp;
-    mp.isBST = lp.isBST && rp.isBST && (node->data >= lp.max && node->data <= rp.min);
-    mp.min = min(node->data, min(lp.min, rp.min));
-    mp.max = max(node->data, max(lp.max, rp.max));
-
-    return mp;
+    return isBST(node->left, minVal, node->data) && isBST(node->right, node->data, maxVal);
 }
 
-int main()
-{
+int main() {
     // Hardcoded tree construction
     Node *root = new Node(50);
     root->left = new Node(25);
     root->right = new Node(75);
-
     root->left->left = new Node(12);
     root->left->right = new Node(37);
-
     root->right->left = new Node(62);
     root->right->right = new Node(87);
 
+    // Display tree structure
+    cout << "Tree structure:" << endl;
+    display(root);
+
     // Check if the tree is a BST
-    BSTPair bp = isBST(root);
-    cout << "Is the tree a BST? " << (bp.isBST ? "Yes" : "No") << endl;
+    bool result = isBST(root, INT_MIN, INT_MAX);
+    cout << "\nIs the tree a BST? " << (result ? "Yes" : "No") << endl;
 
     return 0;
 }
