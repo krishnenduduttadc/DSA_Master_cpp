6f1e88dd0a490afb6c7d2badd40babe908150964
krishnendu <krishnenduduttadc@gmail.com>
2025-03-24 21:58:25 +0530

    addd

--- GraphL3/Dijkstra.cpp
+++ GraphL3/Dijkstra.cpp
@@ -4,50 +4,32 @@ using namespace std;
 class Solution
 {
 public:
-    // Function to find the shortest distance of all the vertices
-    // from the source vertex S.
+
     vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-
-        // Create a priority queue for storing the nodes as a pair {dist,node}
-        // where dist is the distance from source to the node. 
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-        // Initialising distTo list with a large number to
-        // indicate the nodes are unvisited initially.
-        // This list contains distance from source to the nodes.
         vector<int> distTo(V, INT_MAX);
-
-        // Source initialised with dist=0.
         distTo[S] = 0;
-        pq.push({S, 0});
+        pq.push({S, 0}); //src, dist
 
-        // Now, pop the minimum distance node first from the min-heap
-        // and traverse for all its adjacent nodes.
         while (!pq.empty())
         {
             int node = pq.top().first;
             int dis = pq.top().second;
             pq.pop();
 
-            // Check for all adjacent nodes of the popped out
-            // element whether the prev dist is larger than current or not.
             for (auto it : adj[node])
             {
                 int v = it[0];
                 int w = it[1];
-                if (dis + w < distTo[v])
+                if (distTo[v] > dis + w )
                 {
                     distTo[v] = dis + w;
-    
-                    // If current distance is smaller,
-                    // push it into the queue.
                     pq.push({v,dis + w});
                 }
             }
         }
-        // Return the list containing shortest distances
-        // from source to all the nodes.
+
         return distTo;
     }
 };
