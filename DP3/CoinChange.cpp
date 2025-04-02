9935438cfe2c43f11da22518da792d369bb3804c
krishnendu <krishnenduduttadc@gmail.com>
2025-04-03 00:08:14 +0530

    added

--- DP3/CoinChange.cpp
+++ DP3/CoinChange.cpp
@@ -2,16 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to count the number of ways to make change for a given target sum
 long countWaysToMakeChange(vector<int>& arr, int n, int T) {
     vector<vector<long>> dp(n, vector<long>(T + 1, 0)); // Create a DP table
 
-    // Initializing base condition
     for (int i = 0; i <= T; i++) {
         if (i % arr[0] == 0)
             dp[0][i] = 1;
-        // Else condition is automatically fulfilled,
-        // as dp array is initialized to zero
+
     }
 
     for (int ind = 1; ind < n; ind++) {
