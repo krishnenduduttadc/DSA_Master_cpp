2252053618c44a8f74445918df62ad00a4d0c4b2
krishnendu <krishnenduduttadc@gmail.com>
2025-04-11 00:49:18 +0530

    added

--- GraphL3/RottenOranges.cpp
+++ GraphL3/RottenOranges.cpp
@@ -1,81 +1,64 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
   public:
-    //Function to find minimum time required to rot all oranges. 
-    int orangesRotting(vector < vector < int >> & grid) {
-      // figure out the grid size
-      int n = grid.size();
-      int m = grid[0].size();
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        queue<pair<pair<int,int>, int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int fresh = 0;
 
-      // store {{row, column}, time}
-      queue < pair < pair < int, int > , int >> q;
-      int vis[n][m];
-      int cntFresh = 0;
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-          // if cell contains rotten orange
-          if (grid[i][j] == 2) {
-            q.push({{i, j}, 0}); 
-            // mark as visited (rotten) in visited array
-            vis[i][j] = 2;
-          }
-          // if not rotten
-          else {
-            vis[i][j] = 0;
-          }
-          // count fresh oranges
-          if (grid[i][j] == 1) cntFresh++;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 2) {
+                    q.push({{i,j}, 0});
+                    vis[i][j] = 2;
+                }
+                if(grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
         }
-      }
 
-      int tm = 0;
-      // delta row and delta column
-      int drow[] = {-1, 0, +1, 0};
-      int dcol[] = {0, 1, 0, -1}; 
-      int cnt = 0;
+        int time = 0, rotten = 0;
+        int dr[] = {-1, 0, 1, 0}, dc[] = {0, 1, 0, -1};
 
-      // bfs traversal (until the queue becomes empty)
-      while (!q.empty()) {
-        int r = q.front().first.first;
-        int c = q.front().first.second;
-        int t = q.front().second;
-        tm = max(tm, t);
-        q.pop();
-        // exactly 4 neighbours 
-        for (int i = 0; i < 4; i++) {
-          // neighbouring row and column
-          int nrow = r + drow[i];
-          int ncol = c + dcol[i];
-          // check for valid cell and 
-          // then for unvisited fresh orange
-          if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-            vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
-            // push in queue with timer increased
-             q.push({{nrow, ncol}, t + 1}); 
-            // mark as rotten
-            vis[nrow][ncol] = 2;
-            cnt++;
-          }
+        while (!q.empty()) {
+            pair<pair<int, int>, int> front = q.front();
+            q.pop();
+            int r = front.first.first;
+            int c = front.first.second;
+            int t = front.second;
+
+            time = max(time, t);
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
+                    vis[nr][nc] == 0 && grid[nr][nc] == 1) {
+                    q.push({{nr, nc}, t + 1});
+                    vis[nr][nc] = 2;
+                    rotten++;
+                }
+            }
         }
-      }
 
-      // if all oranges are not rotten
-      if (cnt != cntFresh) return -1;
-
-      return tm;
-
+        return (rotten == fresh) ? time : -1;
     }
 };
 
 int main() {
+    vector<vector<int>> grid = {
+        {0, 1, 2},
+        {0, 1, 2},
+        {2, 1, 1}
+    };
 
-  vector<vector<int>>grid{{0,1,2},{0,1,2},{2,1,1}};
-  Solution obj;
-  int ans = obj.orangesRotting(grid);
-  cout << ans << "\n";
+    Solution obj;
+    int ans = obj.orangesRotting(grid);
+    cout << "Answer = " << ans << endl;
 
-  return 0;
+    return 0;
 }
